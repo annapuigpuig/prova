ac1db76120eb8aa2a6be21ba98f5cfe3eb1b3d1d
annapuigpuig <annapuig@ub.edu>
2020-11-26 09:37:47 +0100

    tercer commit

--- exemple/src/MainProva.java
+++ exemple/src/MainProva.java
@@ -1,5 +1,6 @@
 public class MainProva {
     public static void main(String [] args) {
         System.out.println("Hola");
+        System.out.println("Ara canvio una altra cosa");
     }
 }
--- exemple/src/classe3.java
+++ exemple/src/classe3.java
@@ -1,2 +1,6 @@
 public class classe3 {
+
+    public void accio() {
+        System.out.println("mmmmm");
+    }
 }
