e8c9536442f849c5d2c42d2b9d6132baef844e43
annapuigpuig <annapuig@ub.edu>
2020-11-26 09:22:05 +0100

    primer commit

---
+++ exemple/src/MainProva.java
@@ -0,0 +1,5 @@
+public class MainProva {
+    public static void main(String [] args) {
+        System.out.println("Hola");
+    }
+}
---
+++ exemple/src/classeInutil.java
@@ -0,0 +1,4 @@
+public class classeInutil {
+    // classe mai no cridada
+
+}
